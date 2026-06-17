3d9845d3e19b9fce701182797bdd8b2acfc8cf2e
Pruthaviraj Gade <pruthvirajgade2@gmail.com>
2026-06-17 09:50:06 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    with open(file_path, 'r') as file:
+        requirements = file.read().splitlines()
+    return requirements
+
+setup(
+    name='PRODIGY_ML_O1',
+    version='0.1.0',
+    author='Pruthviraj Gade',
+    author_email='pruthvirajgade2@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
